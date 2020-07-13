32fa11db8521b79e72eea96d58078d4d55bd499f
e-mbrown <ebsonari@gmail.com>
2020-07-13 13:47:49 -0700

    Made all my files, added one asset, set up Json and started mobile set up

---
+++ object.js
@@ -0,0 +1,5 @@
+https://docs.google.com/spreadsheets/d/1L-41kQAND4wxOdAVPq1EVSBNMyOmeAmthOdraPwCpRs/edit#gid=0
+
+//take this 1L-41kQAND4wxOdAVPq1EVSBNMyOmeAmthOdraPwCpRs place into bottom link
+
+https://spreadsheets.google.com/feeds/list/1L-41kQAND4wxOdAVPq1EVSBNMyOmeAmthOdraPwCpRs/od6/public/values?alt=json
